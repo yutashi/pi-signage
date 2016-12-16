8a9a81bf3cd87e8bfb86a4b63399425d61019771
vratuy <vratuy@gmail.com>
2016-12-16 10:07:15 +0900

    Set up Babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
   output: {
     path: 'dist',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   }
 };
