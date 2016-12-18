96b2510a2472093bc2db18a55f38a097403ca944
vratuy <vratuy@gmail.com>
2016-12-18 23:44:19 +0900

    Add htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: 'dist',
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
